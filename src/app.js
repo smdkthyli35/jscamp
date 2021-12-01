ee3735590428b90849992b2cd6e1d599444fdf92
zeljkosamed <zeljkosamed@users.noreply.github.com>
2021-12-01 23:54:34 +0300

    js camp 1. gün

---
+++ src/app.js
@@ -0,0 +1,33 @@
+console.log("Merhaba dünya")
+
+let dolarBugun = 14.02
+let dolarDun = 13.20
+
+{
+    let dolarDun = 13.15
+}
+
+console.log(dolarDun)
+
+const euroDun = 14
+//euroDun = 15
+
+console.log(euroDun)
+
+//array
+//camelCasing - kelime ilk harf küçük, diğerleri büyük
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi","Kamu konut kredisi"]
+
+console.log("<ul>")
+
+for(let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>")
+}
+
+
+console.log("</ul>")
+
+
+
+
+console.log(konutKredileri)
